9ed90ff9c0186bd4a81284f28042090ba38da57b
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-24 08:29:18 -0500

    Create nix-backup.sh

---
+++ nix-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# nb stands for nix backup
+
+echo "Installing nix backup"
+
+sudo mkdir /opt/nb
+sudo cp -R /nix /opt/nb
+
+sudo tee /opt/nb/reset-nix <<EOF
+#!/bin/bash
+sudo echo "Resettinging nix..."
+sudo rm -rf /nix/*
+sudo mkdir -p /nix
+sudo cp -R /opt/nb/nix/* /nix/
+sudo echo "Nix has been resetted. Reboot for changes to apply."
+EOF
+
+sudo chmod a+x /opt/nb/reset-nix
+
+sudo echo "Finnished installing nix backup
